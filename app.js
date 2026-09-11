24425bfc243c4cdb173f9f343c681ea5d729e80d
hafsaansar56 <hafsaansar56@gmail.com>
2026-09-11 17:09:14 +0500

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 var student0=["hafsa"]
-var student2=new Array("fatima")
+var student1=new Array("fatima")
+console.log(student0,+student1)
+var students=["hafsa","fatima","aisha","mehmal"]
+var numbers0=[10,20,30,40]
+console.log(students,+numbers0)
+var boolean=(true)
+var mixarr=["hafsa",10,true,undefined,null]
+var quali=["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M.Phil", "PhD"]
+ var number1=[1,2,3,4,5,6,7]
 
+document.write(`"hi this is your subject <br> ${quali} <br> and this is your number ${number1}`)
+
+var players=["micheal","juhn","tony"]
+var numbers2=[320,230,480]
+ document.write(`score of ${players[0]} is ${numbers2[0.] /500 *100} <br>`)
+ document.write(`score of ${players[1]} is ${numbers2[1.] /500 *100} <br> `)
+ document.write(`score of ${players[2]} is ${numbers2[2.] /500 *100} <br> `)
