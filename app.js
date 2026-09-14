d6770bf109f77b3d745d71d314a0edf6789de8dc
hafsaansar56 <hafsaansar56@gmail.com>
2026-09-14 21:37:39 +0500

    commit & push

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ document.write(`"hi this is your subject <br> ${quali} <br> and this is your num
 
 var players=["micheal","juhn","tony"]
 var numbers2=[320,230,480]
- document.write(`score of ${players[0]} is ${numbers2[0.] /500 *100} <br>`)
- document.write(`score of ${players[1]} is ${numbers2[1.] /500 *100} <br> `)
- document.write(`score of ${players[2]} is ${numbers2[2.] /500 *100} <br> `)
+ document.write(`score of ${players[0]} is ${numbers2[0]} Percentage is ${numbers2[0] /500 * 100} <br>`)
+ document.write(`score of ${players[1]} is ${numbers2[1]} Percentage is ${numbers2[1] /500 *100} <br>`)
+ document.write(`score of ${players[2]} is ${numbers2[2]} Percentage is ${numbers2[2] /500 *100} <br>`)
